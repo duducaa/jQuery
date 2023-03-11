ed53e0e95c787618396615765661d1164fbf7298
duducaa <duduca1881@gmail.com>
2023-03-11 15:28:25 -0300

    updated bootstrap

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 $(document).ready(() => {
-    const loginForm = $("form")
-    const loginButton = $(".login-button");
+    const loginForm = $("form");
+
+    const copyButton = $(".copy-button");
 
     loginForm.on("submit", (event) => {
         event.preventDefault();
 
-        $.ajax({
-            url: "http://localhost:5000/login",
-            data: JSON.stringify([{
+        data = JSON.stringify([
+            {
                 "email": $("#email").val(),
                 "str_password": $("#password").val()
-            }]),
+            }
+        ]);
+
+        $.ajax({
             type: "POST",
+            url: "http://localhost:5000/login",
+            data: data,
             dataType: "json",
             contentType: "application/json",
-            success: function (response) {
-                console.log(response);
-            }
+            success: (response) => $("#token").val(response['token'])
         });
     });
 });
